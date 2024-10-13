de1565b62c4cdcbc31c64aacb7720ba554f2dd32
Ramazan <ramazan.du97@gmail.com>
2024-10-13 17:36:26 +0200

    update snake.py

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-from turtle import Turtle,Screen
 
+from turtle import Turtle,Screen
+from snake import Snake
+import time
 screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("PySnake")
+score = 3
+snakeList = []
+screen.tracer(0)
+
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    snake.move()
+    time.sleep(0.1)
+
+
+
+
+#create snake food
+# detect collision with food
+# create a scoreboard
+# detect collision with wall
+# detect collision with tail
+# #
+
+
+
+
+
+
+
+
+screen.exitonclick()
